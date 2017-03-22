1901b4920b0bc04d381d92819047fe0b0e53338e
chrisliu1 <2502380112@qq.com>
2017-03-22 10:04:51 +0800

    new commit

--- Intro2Algorithms/chapter10/SingleLinkedListProblem.java
+++ Intro2Algorithms/chapter10/SingleLinkedListProblem.java
@@ -20,20 +20,24 @@ import java.util.Scanner;
 public class SingleLinkedListProblem {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		/*SingleLinkedList L = new SingleLinkedList(0, null);
+		SingleLinkedList L = new SingleLinkedList(0, null);
 		while(true){
-			System.out.print("Insert is 1, delete is 2:");
-			if(sc.nextInt() == 1){
+			System.out.print("Insert is 1, delete is 2, reverse is 3:");
+			int number = sc.nextInt();
+			if(number == 1){
 				System.out.print("Input the number:");
 				SingleLinkedList Node = new SingleLinkedList(sc.nextInt(), null);
 				L.insert(L, Node);
 			}
-			else{
+			else if(number == 2){
 				System.out.print("Input the number:");
 				L.delete(L, L.search(L, sc.nextInt()));
 			}
+			else{
+				L.reverse();
+			}
 			L.display(L);
-		}*/
+		}
 		/*ListStack stack = new ListStack();
 		while(true){
 			System.out.print("Insert is 1, delete is 2:");
@@ -48,7 +52,7 @@ public class SingleLinkedListProblem {
 			}
 			stack.display(stack.head);
 		}*/
-		ListQueue queue = new ListQueue();
+		/*ListQueue queue = new ListQueue();
 		while(true){
 			System.out.print("Insert is 1, delete is 2:");
 			if(sc.nextInt() == 1){
@@ -61,7 +65,7 @@ public class SingleLinkedListProblem {
 				queue.dequeue();
 			}
 			queue.display();
-		}
+		}*/
 	}
 }
 
@@ -109,6 +113,19 @@ class SingleLinkedList{
 			x = x.next;
 		}
 		System.out.println();
+	}
+	
+	public void reverse(){
+		SingleLinkedList current = null;
+		SingleLinkedList next = this.head;
+		this.head = null;
+		
+		while(next != null){
+	        current = next;
+	        next = next.next;
+	        current.next = this.head;
+	        this.head = current;
+		}
 	}
 }
 
